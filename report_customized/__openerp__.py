8272766a7bc90cd2714b9192005ba17b391cf2d5
Luis M. Ontalba <tecnisagra.ontalba@gmail.com>
2016-10-14 21:59:54 +0200

    Carga inicial

---
+++ report_customized/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Customized report",
+
+    'summary': """
+        Customization of general external report""",
+
+    'description': """
+        This module change header and footer of external report
+    """,
+
+    'author': "Luis Martínez ontalba",
+    'website': "http://www.tecnisagra.com",
+
+    'category': 'Advanced Reporting',
+    'version': '8.0.1.0',
+
+    'depends': ['base','report'],
+
+    'data': [
+        'templates.xml',
+    ]
+}
